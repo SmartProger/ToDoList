66e71b52ec2f4f4dde9ed50e10fd6c330879ff50
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2025-10-22 09:52:13 +0300

    Третий урок

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react"
 import { Form } from "../components/Form/Form"
 import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
+import { ToDo } from "../models/todo-item"
 
 export const ToDoListPage = () => {
+    const [todos, setTodos] = useState<ToDo[]>([
+        {
+            id: 0,
+            text: "Первая задча",
+            isDone: false
+        },
+        {
+            id: 1,
+            text: "Вторая задча",
+            isDone: true
+        },
+        {
+            id: 2,
+            text: "Третья задча",
+            isDone: true
+        }
+    ])
+
+    const createNewToDo = (text: string) => {
+        const newToDo: ToDo = {
+            id: todos.length,
+            text: text,
+            isDone: false
+        }
+        setTodos([...todos, newToDo])
+    }
     return (
         <>
             <Header />
-            <Form />
-            <ToDoList />
+            <Form createNewToDo={createNewToDo} />
+            <ToDoList todos={todos} />
         </>
     )
 }
